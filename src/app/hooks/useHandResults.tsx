b7b7168d99013356226cc440196ed05494b723b3
Paulo Alberto Isidro Rafael Briceno Gonzalez <pabrcn@gmail.com>
2022-07-02 14:22:30 -0300

    improve useHandsResult

--- src/app/hooks/useHandResults.tsx
+++ src/app/hooks/useHandResults.tsx
@@ -1,16 +1,20 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { setHands } from "../handsSlice";
 import handsResult from "../model";
 import { useAppDispatch } from "./../hooks";
 
 const useHandResults = (webcamRef: unknown) => {
   const dispatch = useAppDispatch();
-  useEffect(() => {
+
+  const handResultsAnimation = useCallback(async () => {
     requestAnimationFrame(async () => {
       dispatch(setHands(await handsResult(webcamRef)));
-      useHandResults();
+      handResultsAnimation();
     });
-   
   }, [dispatch, webcamRef]);
+
+  useEffect(() => {
+    handResultsAnimation();
+  }, [handResultsAnimation]);
 };
 export default useHandResults;
