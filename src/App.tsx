e5007917150970eb3fe3cf43fc961d8e49caed0e
Paulo <48759680+pabrcno@users.noreply.github.com>
2024-12-14 20:46:18 -0300

    add start button

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,17 @@ import { Stats } from "@react-three/drei";
 import "./App.css";
 
 import { Hands } from "./components/Hands";
+import { useState } from "react";
 
 function App() {
+  const [showButton, setShowButton] = useState(true);
+
+  const handleStart = () => {
+    console.log("Start button clicked!");
+    setShowButton(false);
+    // Add any logic you want to handle when the button is clicked
+  };
+
   return (
     <div className="App">
       <div className="mainContainer">
@@ -16,6 +25,11 @@ function App() {
 
           <Hands />
         </Canvas>
+        {showButton && (
+          <button className="startButton" onClick={handleStart}>
+            Start
+          </button>
+        )}
       </div>
     </div>
   );
